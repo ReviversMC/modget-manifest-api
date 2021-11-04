162395c9be1e46a45fce2d76d74723911a16082d
Julian Burner <burnerjulian@gmail.com>
2021-11-04 23:00:32 +0100

    Split into different modules (one per spec version)

---
+++ manifest-api-v4/src/test/java/com/github/nebelnidas/modget/manifest_api/TestConfig.java
@@ -0,0 +1,6 @@
+package com.github.nebelnidas.modget.manifest_api;
+
+public class TestConfig {
+	public final static String specV3RepoUrl = "https://raw.githubusercontent.com/NebelNidas/modget-manifests-test-spec-v3";
+	public final static String specV4RepoUrl = "https://raw.githubusercontent.com/NebelNidas/modget-manifests-test-spec-v4";
+}
