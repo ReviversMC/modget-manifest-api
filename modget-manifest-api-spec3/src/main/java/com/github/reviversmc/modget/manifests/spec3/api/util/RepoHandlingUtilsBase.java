f18cc620f691d934b454a96c1289cb3cf827c4b0
NebelNidas <burnerjulian@gmail.com>
2021-12-02 09:05:04 +0100

    Fix `findMaxSharedInt`

--- modget-manifest-api-spec3/src/main/java/com/github/reviversmc/modget/manifests/spec3/api/util/RepoHandlingUtilsBase.java
+++ modget-manifest-api-spec3/src/main/java/com/github/reviversmc/modget/manifests/spec3/api/util/RepoHandlingUtilsBase.java
@@ -5,23 +5,30 @@ import java.util.List;
 public abstract class RepoHandlingUtilsBase {
 
 	protected int findMaxSharedInt(List<Integer> intList1, List<Integer> intList2) {
+        int maxSharedInt = -1;
 		for (int a : intList1) {
+            if (a <= maxSharedInt) continue;
+
 			for (int b : intList2) {
+                if (b <= maxSharedInt) continue;
 				if (a == b) {
-					return a;
+					maxSharedInt = a;
 				}
 			}
 		}
-		return -1;
+		return maxSharedInt;
 	}
 
 	protected int findMaxSharedInt(int a, List<Integer> intList1) {
+        int maxSharedInt = -1;
+
 		for (int b : intList1) {
+            if (b <= maxSharedInt) continue;
 			if (a == b) {
-				return a;
+				maxSharedInt = a;
 			}
 		}
-		return -1;
+		return maxSharedInt;
 	}
 
 }
